7c1e890af694c8f0bbee70e6307ce3b4bda8b67e
cvenn <cv.gh@vennery.com>
2024-02-03 19:51:26 +0000

    Add project files.

---
+++ TitanTest1/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+using LXProtocols.AvolitesWebAPI;
+using LXProtocols.AvolitesWebAPI.Information;
+
+Console.WriteLine("Avo Test");
+
+var titan = new Titan("localhost", Titan.InteractivePort);
+
+//await titan.Selection.SelectFixturesFromHandles(HandleReference.FromUserNumbers(new double[] { 17 }));
+//await titan.Programmer.SetDimmerLevel(100);
+//await titan.Programmer.SetColourControlHSI(0, 1, 1);
+
+//await titan.Selection.SelectFixturesFromHandles(HandleReference.FromUserNumbers(new double[] { 18 }));
+//await titan.Programmer.SetDimmerLevel(100);
+//await titan.Programmer.SetColourControlHSI(60, 1, 1);
+
+//await titan.Selection.SelectFixturesFromHandles(HandleReference.FromUserNumbers(new double[] { 19 }));
+//await titan.Programmer.SetDimmerLevel(100);
+//await titan.Programmer.SetColourControlHSI(120, 1, 1);
+
+//await titan.Selection.SelectFixturesFromHandles(HandleReference.FromUserNumbers(new double[] { 20 }));
+//await titan.Programmer.SetDimmerLevel(100);
+//await titan.Programmer.SetColourControlHSI(180, 1, 1);
+
+//await titan.Selection.SelectFixturesFromHandles(HandleReference.FromUserNumbers(new double[] { 21 }));
+//await titan.Programmer.SetDimmerLevel(100);
+//await titan.Programmer.SetColourControlHSI(240, 1, 1);
+
+//await titan.Selection.SelectFixturesFromHandles(HandleReference.FromUserNumbers(new double[] { 22 }));
+//await titan.Programmer.SetDimmerLevel(100);
+//await titan.Programmer.SetColourControlHSI(300, 1, 1);
+
+//await titan.Selection.SelectFixturesFromHandles(HandleReference.FromUserNumbers(new double[] { 23 }));
+//await titan.Programmer.SetDimmerLevel(100);
+//await titan.Programmer.SetColourControlHSI(360, 1, 1);
+
+await titan.Selection.SelectGroupFromHandle(HandleReference.FromUserNumber(11));
+await titan.Programmer.SetDimmerLevel(100);
+await titan.Programmer.SetColourControlHSI(0, 1, 1);
+
+await titan.Selection.SelectGroupFromHandle(HandleReference.FromUserNumber(12));
+await titan.Programmer.SetDimmerLevel(100);
+await titan.Programmer.SetColourControlHSI(72, 1, 1);
+
+await titan.Selection.SelectGroupFromHandle(HandleReference.FromUserNumber(13));
+await titan.Programmer.SetDimmerLevel(100);
+await titan.Programmer.SetColourControlHSI(144, 1, 1);
+
+await titan.Selection.SelectGroupFromHandle(HandleReference.FromUserNumber(14));
+await titan.Programmer.SetDimmerLevel(100);
+await titan.Programmer.SetColourControlHSI(216, 1, 1);
+
+await titan.Selection.SelectGroupFromHandle(HandleReference.FromUserNumber(15));
+await titan.Programmer.SetDimmerLevel(100);
+await titan.Programmer.SetColourControlHSI(288, 1, 1);
+
+
+//await titan.Programmer.SetControlValue(ControlNames.Tilt, null, 0.2);
+
+await titan.Programmer.SetSelectedDimmerxFade(true);
+
+//await titan.Programmer.LocateSelectedFixtures();
+
+int playbackPage = 0;
+int playbackIndex = 0;
+
+List<HandleInformation> playbacks = (await titan.Playbacks.GetPlaybacks("PlaybackWindow", playbackPage)).ToList();
+if (playbacks.Count == 0 || playbacks[playbackPage].HandleLocation.Index != playbackIndex) {
+    int playbackId = await titan.Playbacks.StoreCue("PlaybackWindow", 0);
+} else {
+    await titan.Playbacks.ReplaceCue(playbacks[playbackPage].TitanId);
+}
+
+await titan.Programmer.ClearAll();
+
+
+//await Task.Delay(3000);
+await titan.Selection.Clear();
+
---
+++ TitanTest2/Program.cs
@@ -0,0 +1,86 @@
+﻿using LXProtocols.AvolitesWebAPI;
+
+namespace TitanTest2 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            Console.WriteLine("Avo Test");
+
+            await (new Tests()).Run();
+        }
+    }
+
+    public class Tests {
+        private Titan titan;
+
+        public Tests() {
+            titan = new Titan("localhost", Titan.InteractivePort);
+//            titan = new Titan("localhost", Titan.NormalPort);
+        }
+
+        public async Task Run() {
+            await titan.Programmer.ClearAll();
+            await Test1();
+            //await Test2();
+        }
+
+        private async Task Test1() {
+            int num = 7;
+            int start = 17;
+            double hue = 0;
+            for (int fixture = start; fixture < start + num; ++fixture, hue += 360 / num) {
+                await AddFixture(fixture, 100, hue, 1, 1);
+            }
+            await titan.Programmer.SetSelectedDimmerxFade(true);
+            await StorePlayback(0, 0);
+
+            await titan.Programmer.ClearAll();
+        }
+
+        private async Task Test2() {
+            int num = 5;
+            int start = 11;
+            double hue = 0;
+            for (int group = start; group < start + num; ++group, hue += 360 / num) {
+                await AddGroup(group, 100, hue, 1, 1);
+            }
+            await titan.Programmer.SetSelectedDimmerxFade(true);
+            await StorePlayback(0, 1);
+
+            await titan.Programmer.ClearAll();
+        }
+
+        private async Task AddFixture(double fixture, double level, double hue, double saturation, double intensity) {
+            await titan.Selection.Clear();
+            await titan.Selection.SelectFixturesFromHandles(HandleReference.FromUserNumbers(new double[] { fixture }));
+            await SetAttributes(level, hue, saturation, intensity);
+        }
+
+        private async Task AddFixtures(double[] fixtures, double level, double hue, double saturation, double intensity) {
+            await titan.Selection.Clear();
+            await titan.Selection.SelectFixturesFromHandles(HandleReference.FromUserNumbers(fixtures));
+            await SetAttributes(level, hue, saturation, intensity);
+        }
+
+        private async Task AddGroup(int group, double level, double hue, double saturation, double intensity) {
+            await titan.Selection.Clear();
+            await titan.Selection.SelectGroupFromHandle(HandleReference.FromUserNumber(group));
+            await SetAttributes(level, hue, saturation, intensity);
+        }
+
+        private async Task SetAttributes(double level, double hue, double saturation, double intensity) {
+            await titan.Programmer.SetDimmerLevel(level);
+            await titan.Programmer.SetColourControlHSI(hue, saturation, intensity);
+        }
+
+        private async Task StorePlayback(int playbackPage, int playbackIndex) {
+            List<HandleInformation> playbacks = (await titan.Playbacks.GetPlaybacks("PlaybackWindow", playbackPage)).ToList();
+            HandleInformation? h = playbacks.FirstOrDefault(p => p.HandleLocation.Index == playbackIndex);
+            if (null == h) {
+                _ = await titan.Playbacks.StoreCue("PlaybackWindow", playbackIndex);
+            } else {
+                await titan.Playbacks.ReplaceCue(h.TitanId);
+            }
+        }
+
+    }
+}
